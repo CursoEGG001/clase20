cd129a58596c099acfdb05c2b739dfbfda0d353b
CursoEGG001 <soygecko@gmail.com>
2024-02-22 01:55:44 -0300

    Vista previa controlador para guardar un prestamo.

--- InitializrSpringbootProject/src/main/java/net/local/demo/Biblio/controladores/PrestamoControlador.java
+++ InitializrSpringbootProject/src/main/java/net/local/demo/Biblio/controladores/PrestamoControlador.java
@@ -5,14 +5,20 @@
 package net.local.demo.Biblio.controladores;
 
 import java.util.List;
+import net.local.demo.Biblio.entidades.Cliente;
+import net.local.demo.Biblio.entidades.Libro;
 import net.local.demo.Biblio.entidades.Prestamo;
 import net.local.demo.Biblio.excepciones.MiExcepcion;
+import net.local.demo.Biblio.servicios.ClienteServicio;
+import net.local.demo.Biblio.servicios.LibroServicio;
 import net.local.demo.Biblio.servicios.PrestamoServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -26,8 +32,40 @@ public class PrestamoControlador {
 
     @Autowired
     PrestamoServicio prestamoServicio;
+    @Autowired
+    ClienteServicio clienteServicio;
+    @Autowired
+    LibroServicio libroServicio;
 
-    public String page(Model model) {
+    @GetMapping("/registra-prestamo")
+    public String cargaPrestamo(Model model) {
+
+        List<Cliente> listaClientes = clienteServicio.ListarClientes();
+        List<Libro> listaLibros = libroServicio.listarLibros();
+        Prestamo prestamo = new Prestamo();
+
+        model.addAttribute("listaClientes", listaClientes);
+        model.addAttribute("listaLibros", listaLibros);
+        model.addAttribute("prestamo", prestamo);
+        return "prestamo_registro";
+    }
+
+    @GetMapping("/registra-prestamo/{id}")
+    public String modificaPrestamo(@PathVariable Long id, Model model) throws MiExcepcion {
+
+        List<Cliente> listaClientes = clienteServicio.ListarClientes();
+        List<Libro> listaLibros = libroServicio.listarLibros();
+        Prestamo prestamo = prestamoServicio.getOne(id);
+
+        model.addAttribute("listaClientes", listaClientes);
+        model.addAttribute("listaLibros", listaLibros);
+        model.addAttribute("prestamo", prestamo);
+        model.addAttribute("attribute", "value");
+        return "prestamo_registro";
+    }
+
+    @PostMapping("/registro")
+    public String guardaPrestamo(Model model) {
         model.addAttribute("attribute", "value");
         return "view.name";
     }
--- InitializrSpringbootProject/src/main/java/net/local/demo/Biblio/servicios/PrestamoServicio.java
+++ InitializrSpringbootProject/src/main/java/net/local/demo/Biblio/servicios/PrestamoServicio.java
@@ -4,7 +4,6 @@
  */
 package net.local.demo.Biblio.servicios;
 
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
